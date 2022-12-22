675789ce9841e8a2f3d9d3570b4c38bff3eddd4d
Evgeniy Shemenev <norfolkevgeniy@gmail.com>
2022-12-23 00:25:56 +0300

    Initial commit

---
+++ src/test/java/org/shemenevweb/magnetism/MagnetismApplicationTests.java
@@ -0,0 +1,13 @@
+package org.shemenevweb.magnetism;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagnetismApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
